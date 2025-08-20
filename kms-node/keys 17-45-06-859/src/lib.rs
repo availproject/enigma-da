125cc3674e412cdfcedc342ed267cdcf0172aa1d
rac-sri <rac.sri25@gmail.com>
2025-08-20 17:34:24 +0530

    feat: update peers.json

---
+++ kms-node/keys 17-45-06-859/src/lib.rs
@@ -0,0 +1,47 @@
+pub mod interface;
+pub mod key_generator;
+pub mod key_type;
+pub mod keys;
+pub mod keystore;
+pub mod scheme_types_imp;
+use std::fmt::{self, Debug, Display};
+#[macro_export]
+macro_rules! unwrap_enum_vec {
+    ($vec:expr, $variant:path, $err:expr) => {{
+        let mut vec = Vec::new();
+        for i in 0..$vec.len() {
+            let val = &$vec[i];
+            match val {
+                $variant(x) => {
+                    vec.push((*x).clone());
+                }
+                _ => Err($err)?,
+            }
+        }
+        Ok(vec)
+    }};
+}
+
+pub enum KeyStoreError {
+    DuplicateEntry(u32),
+    IdMismatch,
+    IdNotFound(u32),
+}
+
+impl Display for KeyStoreError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::DuplicateEntry(id) => write!(f, "Key with id '{}' already exists", id),
+            Self::IdNotFound(id) => {
+                write!(f, "Could not find a key with the given key_id '{}'", id)
+            }
+            KeyStoreError::IdMismatch => write!(f, "Key id does not match key"),
+        }
+    }
+}
+
+impl Debug for KeyStoreError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self, f)
+    }
+}
